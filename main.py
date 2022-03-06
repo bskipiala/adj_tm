49dfc07bdc74e06e395d6f58ee9da27235d417df
bskipiala <bartlomiej.skipiala@edu.uekat.pl>
2022-03-06 18:09:38 +0100

    lab_2

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import re
+from nltk.tokenize import sent_tokenize, word_tokenize
 
-def function1():
+
+def cleanText(input):
+    emoticons = re.findall('[;:][^\w|\s|;|:]?[^\w|\s|;|:]', input)
+    result = input.lower()
+    result = re.sub('\d', '', result)
+    result = re.sub('<[^>]*>', '', result)
+    result = re.sub('[!"#$%&\'()*+,\-./:;<=>?@[\]^_`{|}~]', '', result)
+    result = " ".join(result.split())
+    for emoticon in emoticons:
+        result += emoticon
+    return result
+
+text = "<div><h2>  :)  Header333,,  </h2> <p>article1<b>strong   ;-)   text2!</b> <a href="">link     </a>:(</p></div>"
+cleanedText = cleanText(text)
+print(cleanedText)
+
